00d7723146ff1377ba8a3ba50390f0e08542d3c5
Eva <evaliisagalen@gmail.com>
2019-11-14 10:21:21 +0200

    added redux, react-redux, react-router-dom + initial config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
+import reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('content'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+ReactDOM.render(
+  <Provider store={ createStoreWithMiddleware(reducers) }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  </Provider>, document.getElementById('content'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
