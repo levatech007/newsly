b35e3b24f7a218c87b38945057e746ae45fb7508
Eva <evaliisagalen@gmail.com>
2019-11-14 12:01:52 +0200

    added check if user is logged in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
+  // componentDidMount() {
+    // let user = {email: 'test@test.com', token: 'kje9823bwj'}
+    // localStorage.setItem('user', JSON.stringify(user))
+    // localStorage.removeItem('user')
+  // }
+
   render() {
     return (
-      <div>Newsly</div>
+      <div>
+        {
+          this.props.login.loggedIn ? <p>User existst</p> : <p>user does not exist</p>
+        }
+      </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    login: state.users
+  };
+}
+
+export default connect(mapStateToProps)(App);
