48aeacfe14ab273344f2ddac773c12ec482bec60
Eva <evaliisagalen@gmail.com>
2019-11-15 17:17:13 +0200

    layout and props adjustments

--- src/components/NewsArticles/NewsArticleFull.js
+++ src/components/NewsArticles/NewsArticleFull.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import './news-article.css';
 
 class NewsArticleFull extends Component {
   render() {
+    const currentArticle = this.props.article
     return(
-      <div>
-        NEWS LIST
+      <div className="article">
+        <h1>{ currentArticle.title }</h1>
+        <img src={ currentArticle.urlToImage } alt="#"/>
+        <p>{ currentArticle.author }</p>
+        <p>{ currentArticle.content }</p>
       </div>
     );
   }
--- src/components/NewsArticles/NewsArticlePreview.js
+++ src/components/NewsArticles/NewsArticlePreview.js
@@ -2,21 +2,31 @@ import React, { Component } from 'react';
 import './news-article.css';
 
 class NewsArticlePreview extends Component {
+  constructor() {
+    super();
+    this.handleShowFullArticle = this.handleShowFullArticle.bind(this);
+  }
+
+  handleShowFullArticle(e) {
+    e.preventDefault();
+    console.log("Show article");
+    this.props.onShowArticle(this.props.article);
+  }
 
   render() {
     return(
-      <div className="article-preview">
+      <div className="article preview">
         {
           this.props.isTopArticle ?
             <div>
-            <h1>{ this.props.article.title }</h1>
+            <h1 onClick={ this.handleShowFullArticle }>{ this.props.article.title }</h1>
             <img
               src={ this.props.article.urlToImage }
               alt={ this.props.article.urlToImage }
             />
             </div>
           :
-            <h3>{ this.props.article.title }</h3>
+            <h3 onClick={ this.handleShowFullArticle } >{ this.props.article.title }</h3>
         }
         <p>{ this.props.article.description }</p>
       </div>
