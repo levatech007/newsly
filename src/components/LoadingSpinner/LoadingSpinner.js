22c4fb00f5201ff6afba825c7d8974f93a8c8756
Eva <evaliisagalen@gmail.com>
2019-11-15 12:52:46 +0200

    completed loading spinner

--- src/components/LoadingSpinner/LoadingSpinner.js
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Spinner from './loading-spinner.png';
+import './loading-spinner.css';
 
 class LoadingSpinner extends Component {
+
   render() {
     return(
-      <div>
-        SPINNER
+      <div className="screen">
+        <div className="window">
+          <div className="spinner">
+            <img src={ Spinner } alt="spinner" />
+          </div>
+        </div>
       </div>
     );
   }
