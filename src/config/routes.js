8076c4726b0e7e0829d429b42ae9a0aec44c0dca
Eva <evaliisagalen@gmail.com>
2019-11-15 12:54:26 +0200

    added PageNotFound component to non-declared routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NewsPage from '../containers/NewsPage.js';
 import ProfilePage from '../containers/ProfilePage.js';
+import PageNotFound from '../components/PageNotFound/PageNotFound.js';
 
 export default (
   <Switch>
     <Route exact path="/" component={ NewsPage }/>
     <Route exact path="/profile" component={ ProfilePage }/>
+    <Route path="*" component={ PageNotFound } />
   </Switch>
 )
