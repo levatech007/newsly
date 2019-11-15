2248bb3273b973b9ec7041d9540e959bfb13a808
Eva <evaliisagalen@gmail.com>
2019-11-15 12:56:46 +0200

    style and layout adjustments

--- src/components/NewsArticles/NewsArticlePreview.js
+++ src/components/NewsArticles/NewsArticlePreview.js
@@ -2,27 +2,22 @@ import React, { Component } from 'react';
 import './news-article.css';
 
 class NewsArticlePreview extends Component {
+
   render() {
     return(
       <div className="article-preview">
         {
           this.props.isTopArticle ?
+            <div>
             <h1>{ this.props.article.title }</h1>
-          :
-            <h3>{ this.props.article.title }</h3>
-        }
-
-        <p className="small-text">{ this.props.article.author }</p>
-
-        {
-          this.props.isTopArticle ?
             <img
               src={ this.props.article.urlToImage }
               alt={ this.props.article.urlToImage }
             />
-            :
-            null
-          }
+            </div>
+          :
+            <h3>{ this.props.article.title }</h3>
+        }
         <p>{ this.props.article.description }</p>
       </div>
     );
