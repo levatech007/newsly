44e2f8f4609c8416da051a43980ffe3868481e1c
Eva <evaliisagalen@gmail.com>
2019-11-15 11:14:28 +0200

    edits

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { userActions } from './actions/userActions.js';
 import NavBar from './components/NavBar/NavBar.js';
 import LoginPage from './containers/LoginPage.js';
-import NewsPage from './containers/NewsPage.js';
+import HomePage from './containers/HomePage.js';
 import './App.css';
 
 class App extends Component {
@@ -13,7 +13,6 @@ class App extends Component {
   }
   // pass function to navbar where logout button is located
   handleLogout() {
-    const { dispatch } = this.props;
     this.props.dispatch(userActions.logout());
   }
 
@@ -24,7 +23,7 @@ class App extends Component {
           logout={ this.handleLogout }
           loggedIn={ this.props.login.loggedIn }
         />
-        { this.props.login.loggedIn ? <NewsPage /> : <LoginPage /> }
+        { this.props.login.loggedIn ? <HomePage /> : <LoginPage /> }
       </div>
     );
   }
