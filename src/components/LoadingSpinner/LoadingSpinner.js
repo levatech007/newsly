f358ed49888b76219c41b19037b02ff5f2dc97f7
Eva <evaliisagalen@gmail.com>
2019-11-15 11:14:59 +0200

    created components for 404 and loading

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoadingSpinner extends Component {
+  render() {
+    return(
+      <div>
+        SPINNER
+      </div>
+    );
+  }
+}
+
+export default LoadingSpinner;
