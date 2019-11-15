3feb0756e8a3c53da8af4ea0daef4657729f78ac
Eva <evaliisagalen@gmail.com>
2019-11-15 11:11:35 +0200

    connected to state; imported and implemented NewsArticlePreview

--- src/containers/NewsPage.js
+++ src/containers/NewsPage.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-import MyRoutes from "../config/routes.js"
+import { connect } from 'react-redux';
+import { articleActions } from '../actions';
+import NewsArticlePreview from '../components/NewsArticles/NewsArticlePreview.js';
+import './news-page.css';
 
 class NewsPage extends Component {
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(articleActions.getTopArticles());
+  }
+
   render() {
     return(
-      <div>
-        { MyRoutes }
+      <div className="news-container">
+        <h1>TODAY'S TOP HEADLINES</h1>
+        { this.props.articles.articles &&
+          <ul>
+            {
+              this.props.articles.articles.map((article, idx) => {
+                return(
+                  <NewsArticlePreview
+                    key={ idx }
+                    article={ article }
+                    isTopArticle={ idx ? false : true }
+                  />
+                )
+              })
+            }
+          </ul>
+      }
       </div>
     );
   }
 }
 
-export default NewsPage;
+function mapStateToProps(state) {
+  return {
+    articles: state.articles
+  };
+}
+
+export default connect(mapStateToProps)(NewsPage);
