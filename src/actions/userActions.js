d14f3795b6bf4edbe40a4706d36a987672b58a8c
Eva <evaliisagalen@gmail.com>
2019-11-15 17:15:32 +0200

    enabled user info editing

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -4,7 +4,10 @@ import { history } from '../helpers';
 
 export const userActions = {
   login,
-  logout
+  logout,
+  showEditForm,
+  hideEditForm,
+  editUser
 };
 
 function login(email, token) {
@@ -18,12 +21,26 @@ function login(email, token) {
       };
     };
 
-    function success(user) { return { type: userConstants.LOGIN_SUCCESS, user } }
-    function failure(error) { return { type: userConstants.LOGIN_FAILURE, error } }
+    function success(user) { return { type: userConstants.LOGIN_SUCCESS, user } };
+    function failure(error) { return { type: userConstants.LOGIN_FAILURE, error } };
 }
 
 function logout() {
-  userService.logout()
+  userService.logout();
   history.push('/');
   return { type: userConstants.LOGOUT };
+}
+
+function showEditForm(user) {
+  return { type: userConstants.USER_UPDATE_REQUEST, user };
+}
+
+function hideEditForm(user) {
+  return { type: userConstants.USER_CANCEL_UPDATE_REQUEST, user };
+}
+
+function editUser(email, token) {
+  userService.logout();
+  let user = userService.login(email, token);
+  return { type: userConstants.LOGIN_SUCCESS, user };
 }
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -12,6 +12,24 @@ export function users(state = initialState, action) {
       };
     case userConstants.LOGIN_ERROR:
       return {};
+    case userConstants.USER_UPDATE_REQUEST:
+      return {
+        loggedIn: true,
+        isUpdating: true,
+        user: action.user
+      };
+    case userConstants.USER_CANCEL_UPDATE_REQUEST:
+      return {
+        loggedIn: true,
+        isUpdating: false,
+        user: action.user
+      };
+    case userConstants.USER_UPDATE_INFO_SUCCESS:
+      return {
+        loggedIn: true,
+        isUpdating: false,
+        user: action.user
+      };
     case userConstants.LOGOUT:
       return {};
     default:
